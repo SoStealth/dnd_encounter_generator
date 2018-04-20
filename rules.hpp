b63e9fc2057ca8665cf7246f41aba39fd5a23e5d
Zeren <36263330+SoStealth@users.noreply.github.com>
2018-04-20 15:08:46 +0200

    Create rules.hpp

---
+++ rules.hpp
@@ -0,0 +1,14 @@
+/*
+Author: Lorenzo Damiano, aka Zeren
+Github: https://github.com/SoStealth
+*/
+
+/*
+Rules
+This library contains methods for applying the general D&D rules, like dice throws or modifiers calculator.
+These rules are based on the 3.5 edition of D&D.
+Library will be updated every time a new function for applying a rule will be needed.
+*/
+
+int throw_dice(int);		//Receives number of dice faces
+int get_modifier(int);		//Receives a stat and return modifier
