bc1735113c2d2f480775e54a2a60903e7a408ab0
Zeren <36263330+SoStealth@users.noreply.github.com>
2018-04-20 15:05:03 +0200

    Update classes.hpp

--- classes.hpp
+++ classes.hpp
@@ -1,25 +1,34 @@
-#define N_STATS 8
-#define HP 1
-#define AC 2
-#define STR 3
-#define DEX 4
-#define CON 5
-#define INT 6
-#define WIS 7
-#define CHA 8
-#define MAX_NAME 30
+#define N_STATS 8		//Number of stats for a single entity
+#define HP 1			//Hit points, the "life" of an entity
+#define AC 2			//Armor class, defence of the enemy
+#define STR 3			//Strength attribute
+#define DEX 4			//Dexterity attribute
+#define CON 5			//Constitution attribute
+#define INT 6			//Intelligence attribute
+#define WIS 7			//Wisdom attribute
+#define CHA 8			//Charisma attribute
+
+//MAX constants
+#define MAX_NAME 30		//Maximum length allowed for the entity name
+#define MAX_ARMOR 2		//Max number of armor an entity can have
+#define MAX_ATTACKS 5		//Max number of attacks an entity can have
+
+class Equipment{};
+
+class Armor{};
+
+class Attack{};
 
 class Entity{
 private:char name[MAX_NAME];
-		int stats[N_STATS];
-public:	Entity(filename);
-		~Entity();
-		void set_parameter(char*, int);	//Id, value
-		int get_parameter(char*);	//Id
-		void set_name(char*);
-		char* get_name();
-		int get_modifier(int);
-		int throw(char*, int);	//Id, dice_faces
+	int stats[N_STATS];
+	Armor armors[MAX_ARMOR];
+	Attack attack[MAX_ATTACKS];
+public:	Entity(char*,bool);	//Receives name identifier for the entity OR filename and a boolean to identify which one
+	~Entity();
+	int get_parameter(int);	//Receives parameter identifier
+	char* get_name();	//Receives name of entity
+	int attack
 };
 Entity::Entity(char* filename) {
 	FILE* file = fopen(filename,"r");
