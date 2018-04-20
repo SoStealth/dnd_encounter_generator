05f586d2e878ebdc4c64dd916c112153f2ac2f9b
Zeren <36263330+SoStealth@users.noreply.github.com>
2018-04-20 15:48:29 +0200

    Update rules.hpp

--- rules.hpp
+++ rules.hpp
@@ -10,5 +10,14 @@ These rules are based on the 3.5 edition of D&D.
 Library will be updated every time a new function for applying a rule will be needed.
 */
 
+//Dice constants
+#define D100 100
+#define D20 20
+#define D12 12
+#define D10 10
+#define D8 8
+#define D6 6
+#define D4 4
+
 int throw_dice(int);		//Receives number of dice faces
 int get_modifier(int);		//Receives a stat and return modifier
