79d7574eeb6fff1505aa005b208586e5b327f8a1
Zeren <36263330+SoStealth@users.noreply.github.com>
2018-04-28 08:49:18 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -11,3 +11,17 @@ This library contains functions that will be of use in the code.
 #include <stdio.h>
 #include <string.h>
 
+void get_table(FILE* file, int rows, int columns, int table[rows][columns]) {
+	char* line;
+	size_t len = 0;
+	for(int i=0;i<rows;i++) {
+		getline(&line, &len, file);
+		char* temp;
+		temp = strtok(line,",");
+		for(int j=0;j<columns;j++) {
+			table[i][j] = atoi(temp);
+			temp = strtok(NULL,",");
+		}
+	}
+	return;
+}
