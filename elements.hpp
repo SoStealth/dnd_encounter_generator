3d121008e1f2125a68c7c10aaa7e8a8a0c7b8b17
Zeren <36263330+SoStealth@users.noreply.github.com>
2018-05-18 10:47:02 +0200

    Update elements.hpp

--- elements.hpp
+++ elements.hpp
@@ -15,21 +15,21 @@ This library contains classes used to represent the elements in the game.
 //#include <string.h> is already in "functions.h"
 
 //STATS constants
-#define N_STATS 13		//Number of stats for a single entity
-#define N_BSTATS 9		//Number of stats that cannot be calculated (will be needed in the constructor)
+#define N_STATS 12		//Number of stats serialized for a single entity
 #define LEVEL 0			//Level of the character OR challenge rating of the creature
 #define HP 1			//Hit points, the "life" of an entity
-#define AC 2			//Armor class, defence of the enemy
-#define STR 3			//Strength attribute
-#define DEX 4			//Dexterity attribute
-#define CON 5			//Constitution attribute
-#define INT 6			//Intelligence attribute
-#define WIS 7			//Wisdom attribute
-#define CHA 8			//Charisma attribute
-#define BAB 9			//Base attack bonus attribute
-#define FORT 10			//Base fortitude save attribute
-#define REFL 11			//Base reflexes save attribute
-#define WILL 12			//Base will save attribute
+#define STR 2			//Strength attribute
+#define DEX 3			//Dexterity attribute
+#define CON 4			//Constitution attribute
+#define INT 5			//Intelligence attribute
+#define WIS 6			//Wisdom attribute
+#define CHA 7			//Charisma attribute
+#define BAB 8			//Base attack bonus attribute
+#define FORT 9			//Base fortitude save attribute
+#define REFL 10			//Base reflexes save attribute
+#define WILL 11			//Base will save attribute
+//---------
+#define BASE_AC 10
 
 //MAX constants
 #define MAX_NAME 30		//Maximum length allowed for a name
@@ -150,17 +150,15 @@ Attack::~Attack() {
 int Attack::get_scaling() {
 	return scaling;
 }
-int Attack::attack_roll(int bonus) {
-	int ret;
-	ret = throw_dice(D20) + bonus;
-	return ret;
-}
-int Attack::damage_roll(int bonus, bool is_crit) {
-	int ret;
-	ret = throw_dice(damage);
-	if(is_crit) ret = ret * crit_value;
-	ret = ret + bonus;
-	return ret;
+int Attack::attack_roll(int* attack_roll, int* damage_roll, int bab, int scale) {
+	int roll = throw_dice(D20);
+	int crit = 20 - crit_range;
+	if(roll>crit) {
+		*damage_roll = throw_dice(damage)*crit_value + scale;
+	} else {
+		*damage_roll = throw_dice(damage) + scale;
+	}
+	*attack_roll = roll + bab + scale;
 }
 char* Attack::toString() {
 	char* ret;
@@ -195,9 +193,8 @@ Item::Item(char* s) {
 	temp = strtok(NULL,",");
 	value = atoi(temp);
 	temp = strtok(NULL,",");
-	uses = atoi(temp);
-	temp = strtok(NULL,",");
 	heal = (bool)(temp[0] - '0');		//Apparently subbing '0' to a character makes it an integer
+	uses = throw_dice(D10);
 }
 Item::~Item() {
 	free(name);
@@ -236,15 +233,13 @@ When using a spell, the spell gets sent to the enemy who calculates eventual dam
 class Spell{
 private:char* name;	//Name of the spell
 	int level;	//Level of the spell
-	int type;	//Type of magic of the spell (determines which classes can use it)
 	int value;	//Dice used by the spell to determine the outcome value
-	int save_type;	//Type of saving throw used to be saved from the spell
+	int save_type;	//Type of saving throw used to evade the spell
 	bool heal;	//Determines wether the spell is a healing or damaging one
 public:	Spell(char*);	//Receives a serialized spell string
 	~Spell();
 	char* get_name();
 	int get_level();
-	int get_type();
 	int get_value();
 	int get_save_type();
 	int get_dc(int);	//Receives spellcaster level
@@ -256,8 +251,6 @@ Spell::Spell(char* s) {
 	name = temp;
 	temp = strtok(NULL,",");
 	level = atoi(temp);
-	temp = strtok(NULL,",");
-	type = atoi(temp);
 	temp = strtok(NULL,",");
 	value = atoi(temp);
 	temp = strtok(NULL,",");
@@ -273,9 +266,6 @@ char* Spell::get_name() {
 }
 int Spell::get_level() {
 	return level;
-}
-int Spell::get_type() {
-	return type;
 }
 int Spell::get_uses() {
 	return uses;
@@ -296,7 +286,7 @@ int Spell::get_dc(int caster_level) {
 }
 char* Spell::toString() {
 	char* ret;
-	asprintf(ret,"%s,%d,%d,%d,%d,%d",name,level,type,value,save_type,heal);
+	asprintf(ret,"%s,%d,%d,%d,%d",name,level,value,save_type,heal);
 	return ret;		//FREE
 }
 //----------------------------------------------------------------------------------------------------------------------------------
@@ -315,6 +305,7 @@ public:	Entity(char*);		//Receives serialized entity string
 	~Entity();
 	char* get_name();	//Receives name of entity
 	int get_stat(int);	//Receives parameter identifier
+	int get_ac();
 	int get_current_hp();
 	bool is_alive();	//If current_hp > 0 true, else false
 	bool equip_armor(char*);	//True if armor gets equipped, false if there is no armor space
@@ -330,14 +321,13 @@ public:	Entity(char*);		//Receives serialized entity string
 	bool heal(int);		//Heals the entity by an amount, return false if heal has been neglected
 	/* Act methods: actions an entity can take */
 	bool attack(Entity*,Attack);
-	bool heal(Entity*,value);
 	char* toString();	//serializator
 };
 Entity::Entity(char* s) {
 	char* temp;
 	temp = strtok(s,",");
 	name = temp;
-	for(int i=0;i<N_BSTATS;i++) {		//Only base stats are assigned, the rest can be calculated
+	for(int i=0;i<N_STATS;i++) {		//Only base stats are assigned, the rest can be calculated
 		temp = strtok(NULL,",");
 		stats[i] = atoi(temp);
 	}
@@ -377,6 +367,17 @@ int Entity::get_stat(int id) {
 	} else {
 		return null;
 	}
+}
+int Entity::get_ac() {
+	int ret;
+	ret = BASE_AC;
+	for(i=0;i<MAX_ARMOR;i++) {	//Deletes all armor objects
+		if(armors[i]!=NULL) {
+			ret = ret + armors[i].get_ac_bonus();
+		}
+	}
+	ret = ret + get_modifier(stats[DEX]);
+	return ret;
 }
 int Entity::get_current_hp() {
 	return current_hp;
@@ -539,7 +540,7 @@ public:	Caster(char*);		//Receives serialized caster string
 	bool equip_spell(char*);	//True if spell gets equipped, false if there is no spell space
 	Spell get_spell(int);	//Receives spell identifier
 	bool unequip_spell(int);	//Receives spell identifier, true if spell has been unequipped, false if there is no spell
-	bool cast(bool);	//Receives a boolean that indicates wether the spell will be a healing spell or a damage one
+	bool cast(Entity*,Spell);	//Target, spell
 	char* toString();	//Serializator
 };
 Caster::Caster(char* s) {
@@ -580,6 +581,9 @@ bool Caster::unequip_spell(int id) {
 			return false;
 		}
 	}
+}
+bool Caster::cast(Entity* target, Spell spell) {
+	
 }
 char* Caster::toString() {
 	//this is almost useless because all Caster parameters are calculated, set or generated randomly
