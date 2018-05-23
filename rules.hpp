f706abd9b360b1982c5e59a805fdf229f7eeeb97
Zeren <36263330+SoStealth@users.noreply.github.com>
2018-05-23 09:46:27 +0000

    Update rules.hpp

--- rules.hpp
+++ rules.hpp
@@ -13,6 +13,23 @@ Library will be updated every time a new function for applying a rule will be ne
 #include <time.h>
 #include <stdlib.h>
 
+//STATS constants
+#define N_STATS 12		//Number of stats serialized for a single entity
+#define LEVEL 0			//Level of the character OR challenge rating of the creature
+#define HP 1			//Hit points, the "life" of an entity
+#define STR 2			//Strength attribute
+#define DEX 3			//Dexterity attribute
+#define CON 4			//Constitution attribute
+#define INT 5			//Intelligence attribute
+#define WIS 6			//Wisdom attribute
+#define CHA 7			//Charisma attribute
+#define BAB 8			//Base attack bonus attribute
+#define FORT 9			//Base fortitude save attribute
+#define REFL 10			//Base reflexes save attribute
+#define WILL 11			//Base will save attribute
+//---------
+#define BASE_AC 10
+
 //Dice constants
 #define D100 100
 #define D20 20
