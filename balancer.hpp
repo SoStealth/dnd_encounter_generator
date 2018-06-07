a2fc9dae5eaa0e72dba390a4d05417fb06b89d22
Zeren <36263330+SoStealth@users.noreply.github.com>
2018-06-07 10:16:58 +0000

    Update balancer.hpp

--- balancer.hpp
+++ balancer.hpp
@@ -1,1 +1,30 @@
+#include "entities.hpp"
 
+#define C_VALUE 0
+#define M_VALUE 1
+#define TURNS 2
+#define N_FACTORS 3
+
+#define STRONG_THRESHOLD 4
+#define WEAK_THRESHOLD -4
+
+bool balance(char* balance_value, Entity* *monsters, int n_monsters) {
+	char** temp;
+	int factors[N_FACTORS];
+	split(balance_value,',',&temp);
+	for(int i=0;i<N_FACTORS;i++)
+		factors[i] = atoi(temp[i]);
+	
+	//checking conditions
+	if(factors[C_VALUE]<0) {
+		if(factors[C_VALUE]>STRONG_THRESHOLD) {	//Mostri forti
+			if(factors[C_VALUE]>(STRONG_THRESHOLD*4)) {	//Troppo forti, cambiare mostri
+				//cambiare mostri
+			} else {	//Non così forti, modificare numero o statistiche
+				if(factors[M_VALUE]>WEAK_THRESHOLD) {	//Giocator
+					
+				}
+			}
+		}
+	}
+}
